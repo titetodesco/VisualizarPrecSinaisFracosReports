efa2a070639043bdc2ecd167f7ce700bd464118a
titetodesco <78027430+titetodesco@users.noreply.github.com>
2025-09-01 18:06:19 -0300

    Update novo_app_visualiza.py

--- novo_app_visualiza.py
+++ novo_app_visualiza.py
@@ -406,7 +406,7 @@ else:
                  .agg(Frequencia=("idx_par","count"))
                  .sort_values(["HTO","Frequencia"], ascending=[True,False]))
     st.dataframe(prec_freq, use_container_width=True)
-    st.dataframe(prec_hits[["HTO","Precursor","Similarity","File","Paragraph","Snippet/Trecho"]]
+    st.dataframe(prec_hits[["HTO","Precursor","Similarity","File","Paragraph","Snippet"]]
                  .sort_values("Similarity", ascending=False)
                  .head(200), use_container_width=True)
 
